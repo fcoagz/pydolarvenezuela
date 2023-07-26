d3bddb9909e10ddbfa8f9ed45175121de4889f9b
fcoagz <grihardware@gmail.com>
2023-07-26 00:05:58 -0400

    adding the pyBCV module

--- pyDolarVenezuela/__init__.py
+++ pyDolarVenezuela/__init__.py
@@ -1,2 +1,2 @@
-from .scraping_bcv import Bcv
-from .scraping_monitor import Monitor
+from pyDolarVenezuela.module_bcv import Bcv
+from pyDolarVenezuela.scraping_monitor import Monitor
---
+++ pyDolarVenezuela/module_bcv.py
@@ -0,0 +1,8 @@
+from pyBCV import Currency
+from decimal import Decimal
+
+class Bcv(object):
+    def get_rates(self, currency_code: str, prettify: bool = True):
+        round_number_rate = Decimal(Currency().get_rate(currency_code=currency_code, prettify=False))
+        round_number_rate = round(round_number_rate, 2) 
+        return round_number_rate if not prettify else f"Bs. {round_number_rate}"
