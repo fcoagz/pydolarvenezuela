ad4fed7dc66c0322f894b34b15bdca330d064f06
fcoagz <grihardware@gmail.com>
2023-07-26 19:33:31 -0400

    will return the date value of the current dollar price

--- pyDolarVenezuela/scraping_monitor.py
+++ pyDolarVenezuela/scraping_monitor.py
@@ -1,10 +1,14 @@
 from bs4 import BeautifulSoup
+import re
 
 from pyDolarVenezuela.request import get_content_page
 from pyDolarVenezuela.util import PAGINA_PRINCIPAL_EXCHANGE_MONITOR
 
 def _get_values_monitors(soup: BeautifulSoup):
     return [value for value in soup]
+
+def _get_date_value(soup: BeautifulSoup):
+    return str(soup.find('p')).split("<br/>")[-1].replace("</p>", "")
 
 class Monitor(object):
     """
@@ -15,9 +19,13 @@ class Monitor(object):
     def _load(self):
         soup = BeautifulSoup(get_content_page(PAGINA_PRINCIPAL_EXCHANGE_MONITOR), "html.parser")
         section_dolar_venezuela = soup.find_all("div", "col-xs-12 col-sm-6 col-md-4 col-tabla")
+        section_fecha_valor = soup.find("div", "col-xs-12 text-center")
+
+        date = _get_date_value(section_fecha_valor)
         all_monitors = _get_values_monitors(section_dolar_venezuela)
 
         self.all_monitors = {}
+        self.all_monitors['value'] = {"date": date}
         i: int = 0
         for monitor in all_monitors:
             monitor = monitor.find("div", "module-table module-table-fecha")
@@ -27,10 +35,10 @@ class Monitor(object):
                 "price": str(monitor.find('p', 'precio').text).replace(',', '.'),
                 "last_update": monitor.find('p', 'fecha').text
             }
-            self.all_monitors[i] = data
+            self.all_monitors[f"{i}"] = data
             i += 1
     
-    def get_value_monitors(self, monitor_code: int = None, name_property: str = None, prettify: bool = False):
+    def get_value_monitors(self, monitor_code: str = None, name_property: str = None, prettify: bool = False):
         """
         El parámetro `monitor_code` indica el código del monitor del cual se desea obtener información, \
         mientras que el parámetro `prettify` permite mostrar los precios en formato de moneda con el símbolo de Bolívares. \
