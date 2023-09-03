e1e8dba8cbe421615ac2fc52eaee4d4fb4125444
fcoagz <grihardware@gmail.com>
2023-09-03 13:55:11 -0400

    ~

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '1.1.5'
+VERSION = '1.1.6'
 PACKAGE_NAME = 'pyDolarVenezuela' 
 AUTHOR = 'Francisco Griman'
 AUTHOR_EMAIL = 'grihardware@gmail.com'
@@ -18,7 +18,8 @@ INSTALL_REQUIRES = [
       'requests',
       'bs4',
       'beautifulsoup4',
-      'pyBCV'
+      'pyBCV',
+      'babel'
       ]
 
 CLASSIFIERS = [
