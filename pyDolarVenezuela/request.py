2c4d6ddd3c423b735c6c349f5b219f4741b20c2b
fcoagz <grihardware@gmail.com>
2023-09-06 18:49:04 -0400

    removing unused code

--- pyDolarVenezuela/request.py
+++ pyDolarVenezuela/request.py
@@ -6,10 +6,6 @@ def _ensure_200_and_return_content(response: Response) -> bytes:
         raise ValueError("")
     return response.content
 
-# def ask(url: str, params: dict):
-#     response = httpx.get(url, params=params, timeout=10.0)
-#     return _ensure_200_and_return_content(response)
-
 def content(url: str) -> bytes:
     response = requests.get(url, timeout=10.0)
     return _ensure_200_and_return_content(response)
