e3dcc63804ebc1fa0407643fad152a79adc76a54
fcoagz <grihardware@gmail.com>
2023-09-06 13:15:49 -0400

    page being scraped

--- pyDolarVenezuela/util.py
+++ pyDolarVenezuela/util.py
@@ -1,2 +1,2 @@
-PAGINA_PRINCIPAL_EXCHANGE_MONITOR = "https://exchangemonitor.net/dolar-venezuela"
-ZONA_HORARIA_VENEZUELA = "http://worldtimeapi.org/api/timezone/America/Caracas"
+ExchangeMonitor = "https://exchangemonitor.net"
+WorldTime = "http://worldtimeapi.org/api/timezone/America/Caracas"
