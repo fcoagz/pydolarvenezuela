a393733e67713982cc2570848426f674d473938d
fcoagz <grihardware@gmail.com>
2023-09-07 12:27:12 -0400

    requests were replaced by httpx for faster data collection

--- pyDolarVenezuela/request.py
+++ pyDolarVenezuela/request.py
@@ -1,11 +1,11 @@
-from requests import Response
-import requests
+from httpx import Response
+import httpx
 
 def _ensure_200_and_return_content(response: Response) -> bytes:
-    if response.status_code != requests.codes.ok:
-        raise ValueError("")
+    if response.status_code != httpx.codes.OK:
+        raise ValueError(f"I can't communicate with the Exchange Monitor page {response.status_code}")
     return response.content
 
 def content(url: str) -> bytes:
-    response = requests.get(url, timeout=10.0)
+    response = httpx.get(url, timeout=10.0)
     return _ensure_200_and_return_content(response)
