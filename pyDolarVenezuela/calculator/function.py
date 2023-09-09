68c73ae39a4759dc4d0f3c142b402767a35ced2a
fcoagz <grihardware@gmail.com>
2023-09-09 09:15:39 -0400

    new function a currency converter

---
+++ pyDolarVenezuela/calculator/function.py
@@ -0,0 +1,23 @@
+def _bs_to_dollar(value: float, price_monitor: float):
+    return value / price_monitor
+
+def _dollar_to_bs(value: float, price_monitor: float):
+    return value * price_monitor
+
+def _isnumber(_object):
+    if isinstance(_object, int) or isinstance(_object, float):
+        return True
+    return False
+
+def currency_converter(type: str, value, monitor: dict):
+    price_monitor = monitor['price']
+
+    if _isnumber(value):
+        if type == 'VES':
+            return _bs_to_dollar(value, float(price_monitor))
+        elif type == 'USD':
+            return _dollar_to_bs(value, float(price_monitor))
+        else:
+            raise ValueError(f"The type must be USD or VES not {type}")
+    else:
+        raise TypeError(f"The value must be an integer or floating point number, not {value}")
