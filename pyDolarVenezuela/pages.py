512bd32f58fbdd975449bb0ae734c47746446983
fcoagz <grihardware@gmail.com>
2023-09-29 19:41:02 -0400

    monitors

---
+++ pyDolarVenezuela/pages.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+@dataclass
+class Monitor:
+    name: str
+    provider: str
+
+BCV = Monitor(
+    name="Banco Central de Venezuela",
+    provider="http://www.bcv.org.ve/"
+)
+
+CriptoDolar = Monitor(
+    name="Cripto Dolar",
+    provider="https://exchange.vcoud.com/"
+)
+
+ExchangeMonitor = Monitor(
+    name="Exchange Monitor",
+    provider="https://exchangemonitor.net/"
+)
