a394fa9159889584a82ad6b047953b93ad93c057
fcoagz <grihardware@gmail.com>
2023-09-29 19:43:21 -0400

    extraction of current time

---
+++ pyDolarVenezuela/tools/time.py
@@ -0,0 +1,21 @@
+from datetime import datetime 
+from babel.dates import format_date, format_time, get_timezone
+
+class TimeDollar(object):
+    def __init__(self) -> None:
+        self.locality = get_timezone('America/Caracas')
+        self.datetime_obj = datetime.now(self.locality)
+    
+    def get_time(self, date: str):
+        datetime_obj = datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%fZ')
+        
+        return datetime_obj.strftime("%d/%m/%Y, %I:%M %p")
+
+    def get_time_zone(self):
+        formatted_date = format_date(self.datetime_obj.date(), format='full', locale='es')
+        formatted_time = format_time(self.datetime_obj.time(), format='h:mm:ss a', locale='es')
+
+        return {
+            "date": formatted_date,
+            "time": formatted_time
+        }
