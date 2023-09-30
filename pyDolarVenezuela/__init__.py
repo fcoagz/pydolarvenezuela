4db8bd0b496000be82d2eac85623a32a69f0bb4b
fcoagz <grihardware@gmail.com>
2023-09-30 14:17:31 -0400

    ~

--- pyDolarVenezuela/__init__.py
+++ pyDolarVenezuela/__init__.py
@@ -6,7 +6,7 @@ from .provider import Provider
 import json
 from colorama import Fore
 
-version = '1.3.1'
+version = '1.3.2'
 
 def check_dependence_version():
     response = network.get("https://pypi.org/pypi/pydolarvenezuela/json")
