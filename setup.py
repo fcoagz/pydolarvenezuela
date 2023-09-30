826efdbc5b3343457138803b8c2c4bb04c9cc92f
fcoagz <grihardware@gmail.com>
2023-09-29 22:09:52 -0400

    the best version 1.3.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '1.3.0'
+VERSION = '1.3.1'
 PACKAGE_NAME = 'pyDolarVenezuela' 
 AUTHOR = 'Francisco Griman'
 AUTHOR_EMAIL = 'grihardware@gmail.com'
