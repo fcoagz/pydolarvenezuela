64babb2b8d281b161e1d8c7a27f4e73c1697e841
fcoagz <grihardware@gmail.com>
2023-10-03 18:45:51 -0400

    including `Dpedidos`

--- pyDolarVenezuela/pages.py
+++ pyDolarVenezuela/pages.py
@@ -24,3 +24,8 @@ iVenezuela = Monitor(
     name="iVenezuela",
     provider="https://www.ivenezuela.travel/"
 )
+
+Dpedidos = Monitor(
+    name="Monitor Dolar Venezuela",
+    provider="https://api.lyldesarrollo.com/"
+)
