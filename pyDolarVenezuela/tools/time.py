3a9725eacf7a168f403e935106cdaddccd356d04
fcoagz <grihardware@gmail.com>
2023-10-03 18:46:23 -0400

    modifying the code of the `time` module

--- pyDolarVenezuela/tools/time.py
+++ pyDolarVenezuela/tools/time.py
@@ -6,11 +6,14 @@ class TimeDollar(object):
         self.locality = get_timezone('America/Caracas')
         self.datetime_obj = datetime.now(self.locality)
     
-    def get_time(self, date: str):
-        datetime_obj = datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%fZ')
+    def get_time(self, date: str, dateofApi: bool = False):
+        if dateofApi:
+            datetime_obj = datetime.strptime(date, '%Y-%m-%d %H:%M')
+        else:
+            datetime_obj = datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%fZ')
         
         return datetime_obj.strftime("%d/%m/%Y, %I:%M %p")
-
+    
     def get_time_zone(self):
         formatted_date = format_date(self.datetime_obj.date(), format='full', locale='es')
         formatted_time = format_time(self.datetime_obj.time(), format='h:mm:ss a', locale='es')
