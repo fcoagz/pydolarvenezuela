cc35b618330e540004d4fcf675798daa0b894c53
fcoagz <grihardware@gmail.com>
2023-10-06 13:23:17 -0400

    time correction

--- pyDolarVenezuela/tools/time.py
+++ pyDolarVenezuela/tools/time.py
@@ -1,16 +1,19 @@
-from datetime import datetime 
+from datetime import datetime, timedelta
 from babel.dates import format_date, format_time, get_timezone
+from pytz import timezone
 
 class TimeDollar(object):
     def __init__(self) -> None:
-        self.locality = get_timezone('America/Caracas')
+        self.locality = timezone('America/Caracas')
         self.datetime_obj = datetime.now(self.locality)
     
     def get_time(self, date: str, dateofApi: bool = False):
         if dateofApi:
             datetime_obj = datetime.strptime(date, '%Y-%m-%d %H:%M')
         else:
-            datetime_obj = datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%fZ')
+            duration = timedelta(hours=4)
+            zona_horaria_universal = datetime.fromisoformat(date[:-1])
+            datetime_obj = zona_horaria_universal - duration
         
         return datetime_obj.strftime("%d/%m/%Y, %I:%M %p")
     
