61a5bd426b9261aff8dd389c30ddac34e04d0a01
fcoagz <grihardware@gmail.com>
2024-04-25 19:45:05 -0400

    add class pages

---
+++ pyDolarVenezuela/pages.py
@@ -0,0 +1,31 @@
+from .models.pages import Monitor
+
+BCV = Monitor(
+    name="Banco Central de Venezuela",
+    provider="http://www.bcv.org.ve/",
+    currencies=['usd', 'eur', 'cny', 'try', 'rub']
+)
+
+CriptoDolar = Monitor(
+    name="Cripto Dolar",
+    provider="https://exchange.vcoud.com/",
+    currencies=['usd', 'eur']
+)
+
+ExchangeMonitor = Monitor(
+    name="Exchange Monitor",
+    provider="https://exchangemonitor.net/",
+    currencies=['usd', 'eur']
+)
+
+iVenezuela = Monitor(
+    name="iVenezuela",
+    provider="https://www.ivenezuela.travel/",
+    currencies=['usd']
+)
+
+Dpedidos = Monitor(
+    name="Monitor Dolar Venezuela",
+    provider="https://api.lyldesarrollo.com/",
+    currencies=['usd']
+)
