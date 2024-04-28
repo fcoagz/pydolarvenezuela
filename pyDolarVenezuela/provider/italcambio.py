6cc62c156cf357a1c51dfdcb731fc1ef1170fb56
fcoagz <grihardware@gmail.com>
2024-04-28 17:34:55 -0400

    add date obtained

--- pyDolarVenezuela/provider/italcambio.py
+++ pyDolarVenezuela/provider/italcambio.py
@@ -1,6 +1,8 @@
-from ..network import get
-from ..utils import code_currencies
+from datetime import datetime
 from bs4 import BeautifulSoup
+from ..network import get
+from ..tools.time import standard_time_zone
+from ..utils import code_currencies
 
 class Italcambio:
     def __init__(self, url: str, **kwargs) -> None:
@@ -17,11 +19,14 @@ class Italcambio:
                 title = code_currencies[monitors_amounts[i]]
                 price_old = float(str(monitors_amounts[i-1]).split()[-1])
                 price = round(price_old, 2)
+                dt = datetime.now(standard_time_zone)
+                dt_tostring = dt.strftime('%d/%m/%Y, %I:%M %p')
     
                 self.rates[monitors_amounts[i].lower()] = {
                     'title': title,
                     'price': price,
-                    'price_old': price_old
+                    'price_old': price_old,
+                    'last_update': dt_tostring
                 }
     
     def get_values(self):
