37bb9a3e24af153cf840cdf8cc43dfad3649705a
fcoagz <grihardware@gmail.com>
2024-05-19 03:37:43 -0400

    VERSION: 1.5.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '1.5.6'
+VERSION = '1.5.7'
 PACKAGE_NAME = 'pyDolarVenezuela' 
 AUTHOR = 'Francisco Griman'
 AUTHOR_EMAIL = 'grihardware@gmail.com'
