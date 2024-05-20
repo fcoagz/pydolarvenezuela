f04b3d2cf5310fb207cc1d2e389973c2a51f951c
fcoagz <grihardware@gmail.com>
2024-05-20 14:42:43 -0400

    VERSION: 1.5.8

--- pyDolarVenezuela/__init__.py
+++ pyDolarVenezuela/__init__.py
@@ -8,7 +8,7 @@ from .data.redis import Redis
 from .tools import get_time_zone as getdate, currency_converter
 from .provider import select_monitor
 
-version = '1.5.7'
+version = '1.5.8'
 """
 Versión actual de la biblioteca    
 """
