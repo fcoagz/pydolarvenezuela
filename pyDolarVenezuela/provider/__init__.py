dc9daba06434a2bc98b602b7996d29b3118d393b
fcoagz <grihardware@gmail.com>
2024-05-25 22:12:34 -0400

    Correction in the code and improvement in the structure

--- pyDolarVenezuela/provider/__init__.py
+++ pyDolarVenezuela/provider/__init__.py
@@ -7,139 +7,131 @@ from .exchangemonitor import ExchangeMonitor
 from .italcambio import Italcambio
 
 from ..data.redis import Cache
-from ..models.monitor import MonitorDB
+from ..models.monitor import Monitor
 from ..models.database import Redis
 from ..models.pages import Page
 from ..pages import BCV as B, CriptoDolar as C, ExchangeMonitor as E, Italcambio as I
-from ..utils import currencies_list
 
-monitor_classes = [
-    {
-        B.name: {
-            'currency': B.currencies,
-            'provider': BCV
-        }
-    },
-    {
-        C.name: {
-            'currency': C.currencies,
-            'provider': CriptoDolar
-        }
-    },
-    {
-        E.name: {
-            'currency': E.currencies,
-            'provider': ExchangeMonitor
-        }
-    },
-    {
-        I.name: {
-            'currency': I.currencies,
-            'provider': Italcambio
-        }
-    }
-]
+monitor_classes = {
+    B.name: {'currency': B.currencies, 'provider': BCV},
+    C.name: {'currency': C.currencies, 'provider': CriptoDolar},
+    E.name: {'currency': E.currencies, 'provider': ExchangeMonitor},
+    I.name: {'currency': I.currencies, 'provider': Italcambio},
+}
 
-def select_monitor(provider: Page, db: Redis, **kwargs):
-    global data
+class Provider:
+    def __init__(self,
+                page: Page, 
+                currency: str, 
+                db: Redis = None) -> None:
+        if currency.lower() not in page.currencies:
+            raise ValueError(f"The currency type must be 'usd', 'eur'..., not {currency}")
         
-    currency = kwargs.get('currency', None)
-    monitor_code = kwargs.get('monitor_code', None)
-    name_property = kwargs.get('name_property', None)
-    prettify = kwargs.get('prettify', False)
+        self.page = page
+        self.currency = currency.lower()
+        self.db = db
 
-    if currency not in currencies_list:
-        raise ValueError(f"The currency type must be 'usd', 'eur'..., not {currency}")
+        if self.db is not None:
+            self._redis = Cache(self.db)
 
-    def _get_values_specifics():
-        if not monitor_code:
-            return data
-        
-        try:
-            monitor_data = data[monitor_code.lower()]
-            if name_property:
-                if name_property == 'last_update' and provider.name == B.name:
-                    return data[name_property]
-                value = monitor_data[name_property]
-                return f'Bs. {value}' if prettify and name_property == 'price' else value
-            return monitor_data
-        except KeyError:
-            raise KeyError("Does not match any of the properties that were provided in the dictionary. Most information: https://github.com/fcoagz/pyDolarVenezuela")
+    def _load_data(self):
+        monitor_class = monitor_classes.get(self.page.name).get('provider')
+        values = monitor_class(url=self.page.provider, currency=self.currency).get_values()
 
-    def _update_item(existing_data_dict: dict, i: Any, last_data: dict):
-        structure_monitor = asdict(MonitorDB(**existing_data_dict[i]))
-        for key in list(existing_data_dict[i].keys()):
+        if self.db is not None:
+            key = f'{self.currency}:{self.page.name}'
+            if not self._redis.get_data(key):
+                self._redis.set_data(key, json.dumps(values), self.db.ttl)
+
+            old_data = json.loads(self._redis.get_data(key))
+            for property in old_data:
+                if property not in ('banks', 'last_update'):
+                    try:
+                        self._update_item(old_data, values, property)
+                    except KeyError:
+                        pass
+                elif property == 'banks': # Comparación de propiedades de los datos BCV
+                    banks = values[property]
+                    for i, bank in enumerate(old_data[property]):
+                        if i < len(banks) and bank['title'] == banks[i]['title']:
+                            self._update_item(old_data[property], banks, i)
+                elif property == 'last_update':
+                    old_data[property] = values[property]
+
+            self._redis.set_data(key, json.dumps(values), self.db.ttl)
+            values = json.loads(self._redis.get_data(key))
+            
+        return values 
+    
+    def _update_item(self, old_data: dict, new_data: dict, i: Any):
+        """
+        Evalúa la estructura de cada monitor en `old_data`. Elimina los atributos que sean `None` (Cada estructura es diferente según el proveedor) y realiza los cálculos necesarios.
+
+        Estructura inicial (para la primera vez):
+        ```python
+        class Monitor:
+            title: str  
+            price: float  
+            price_old: Optional[float] = None 
+            last_update: Optional[str] = None  
+            image: Optional[str] = None  
+            percent: Optional[float] = 0.0
+            change: Optional[float] = 0.0  
+            color: Optional[str] = "neutral" 
+            symbol: Optional[str] = "" 
+        ```
+        """
+        structure_monitor = asdict(Monitor(**old_data[i]))
+        for key in list(old_data[i].keys()):
             if structure_monitor[key] is None:
-                del existing_data_dict[i][key]
+                del old_data[i][key]
         
-        if existing_data_dict[i]['price'] != last_data[i]['price']:
-            price = existing_data_dict[i]['price']
-            new_price = last_data[i]['price']
-            price_old = last_data[i].get('price_old', None)
-            change  = round(float(new_price - price), 2)
-            percent = float(f'{round(float((change / new_price) * 100 if price != 0 else 0), 2)}'.replace('-', ' '))
-            symbol  = "" if change == 0 else "▲" if change >= 0 else "▼"
-            color   = "red" if symbol == '▼' else "green" if symbol == '▲' else "neutral"
-            last_update = last_data[i].get('last_update', None)
+        if old_data[i]['price'] != new_data[i]['price']:
+            old_price = old_data[i]['price']
+            new_price = new_data[i]['price']
+            price_old = new_data[i].get('price_old', None) # Valor preciso
+            change    = round(float(new_price - old_price), 2)
+            percent   = float(f'{round(float((change / new_price) * 100 if old_price != 0 else 0), 2)}'.replace('-', ' '))
+            symbol    = "" if change == 0 else "▲" if change >= 0 else "▼"
+            color     = "red" if symbol == '▼' else "green" if symbol == '▲' else "neutral"
+            last_update = new_data[i].get('last_update', None)
+
             change = float(str(change).replace('-', ' '))
 
-            existing_data_dict[i].update({
-                    'price': new_price,
-                    'change': change,
-                    'percent': percent,
-                    'color': color,
-                    'symbol': symbol,
+            old_data[i].update({
+                'price': new_price,
+                'change': change,
+                'percent': percent,
+                'color': color,
+                'symbol': symbol,
             })
-    
-            if last_update and price_old is not None:
-                existing_data_dict[i].update({
+            
+            # Comprueba si los atributos tienen valor. se agregan y/o actualizan
+            if last_update and price_old:
+                old_data[i].update({
                     'price_old': price_old,
                     'last_update': last_update
                 })
-            elif last_update is not None:
-                existing_data_dict[i].update({
+                
+            elif last_update: 
+                old_data[i].update({
                     'last_update': last_update
                 })
 
-    try:
-        monitor_class = None
-
-        for monitor in monitor_classes:
-            if monitor.get(provider.name) and currency in monitor.get(provider.name)['currency']:
-                monitor_class = monitor[provider.name]['provider']
-
-        if monitor_class is not None:
-            data = monitor_class(url=provider.provider, currency=currency).get_values()
-            if db is not None:
-                key = f'{currency}:{provider.name}'
-                cache = Cache(db)
-                
-                existing_data = cache.get_data(key)
-
-                if not existing_data:
-                    cache.set_data(key, json.dumps(data), db.ttl)
-
-                existing_data_dict = json.loads(cache.get_data(key))
-                for name in existing_data_dict:
-                    if not name == 'last_update' and not name == 'banks':
-                        if name in existing_data_dict and name in data:
-                            _update_item(existing_data_dict, name, data)
-                    else:
-                        if name == 'banks':
-                            for i, bank in enumerate(existing_data_dict[name]):
-                                if i < len(data[name]) and bank['title'] == data[name][i]['title']:
-                                    _update_item(existing_data_dict[name], i, data[name])
-                        elif name == 'last_update':
-                            existing_data_dict[name] = data[name]
-                            
-                cache.set_data(key, json.dumps(existing_data_dict), db.ttl)
-                data = json.loads(
-                    cache.get_data(key)
-                )
-
-                return _get_values_specifics()
-            return _get_values_specifics()
-        else:
-            raise ValueError("Provider not supported")
-    except Exception as e:
-        raise e
+    def _get_values_specifics(self, type_monitor: str = None, property: str = None, prettify: bool = False):
+        data = self._load_data()
+        if not type_monitor:
+            return data
+        
+        try:
+            monitor_data = data[type_monitor.lower()]
+            if property:
+                if property == 'last_update' and self.page.name == B.name:
+                    return monitor_data[property]
+                value = monitor_data[property]
+                return f'Bs. {value}' if prettify and property == 'price' else value
+            return monitor_data
+        except KeyError:
+            raise KeyError('Property not found. https://github.com/fcoagz/pyDolarVenezuela')
+        except Exception as e:
+            raise e
