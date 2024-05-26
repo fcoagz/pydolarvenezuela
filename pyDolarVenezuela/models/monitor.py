92e68a1b9f19e2de9cccbbbc3b174bb4d8a04535
fcoagz <grihardware@gmail.com>
2024-05-25 21:48:44 -0400

    Change structure model Monitor

--- pyDolarVenezuela/models/monitor.py
+++ pyDolarVenezuela/models/monitor.py
@@ -2,7 +2,7 @@ from typing import Optional
 from dataclasses import dataclass
 
 @dataclass
-class MonitorDB:
+class Monitor:
     """
     MonitorDB instance
     """
@@ -10,11 +10,11 @@ class MonitorDB:
     price: float  
     price_old: Optional[float] = None 
     last_update: Optional[str] = None  
-    percent: Optional[float] = None 
-    change: Optional[float] = None  
-    color: Optional[str] = None  
-    symbol: Optional[str] = None 
     image: Optional[str] = None  
+    percent: Optional[float] = 0.0
+    change: Optional[float] = 0.0  
+    color: Optional[str] = "neutral" 
+    symbol: Optional[str] = "" 
 
     def __str__(self):
         return f"{self.__class__.__name__}(title={self.title}, price={self.price}, price_old={self.price_old}, last_update={self.last_update}, percent={self.percent}, change={self.change}, color={self.color}, symbol={self.symbol}, image={self.image})"
