ed45e85d2eff33f9c2c3922e8d8c869631ed9d0f
fcoagz <grihardware@gmail.com>
2024-06-22 14:12:36 -0400

    Add raise KeyError

--- pyDolarVenezuela/utils/calculator.py
+++ pyDolarVenezuela/utils/calculator.py
@@ -4,7 +4,9 @@ def currency_converter(type: Literal['VES', 'USD', 'EUR'], value, monitor: dict)
     """
     Convierte una cantidad de dinero de una moneda a otra utilizando los datos de un monitor específico.
     """
-    price_monitor = monitor['price']
+    price_monitor = monitor.get('price')
+    if not price_monitor:
+        raise KeyError('The monitor was not found')
 
     try:
         if isinstance(value, int) or isinstance(value, float):
