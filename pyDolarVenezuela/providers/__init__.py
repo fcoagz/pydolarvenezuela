91bfd98e177d38f039b4ed49f73be121cb5d4e59
Francisco Griman <grihardware@gmail.com>
2024-07-11 18:33:43 -0400

    Add DolarToday

--- pyDolarVenezuela/providers/__init__.py
+++ pyDolarVenezuela/providers/__init__.py
@@ -1,5 +1,6 @@
 from .alcambio import AlCambio
 from .bcv import BCV
 from .criptodolar import CriptoDolar
+from .dolartoday import DolarToday
 from .exchangemonitor import ExchangeMonitor
 from .italcambio import Italcambio
