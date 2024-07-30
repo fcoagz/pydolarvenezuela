90954cf537c0c54daa1afd744146a659fd5e9634
Francisco Griman <grihardware@gmail.com>
2024-07-30 17:04:31 -0400

    Update version to 1.8.2

--- pyDolarVenezuela/version.py
+++ pyDolarVenezuela/version.py
@@ -2,7 +2,7 @@ import json
 from colorama import Fore
 from . import network
 
-__version__ = '1.8.1'
+__version__ = '1.8.2'
 """
 Versión actual de la biblioteca    
 """
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '1.8.1'
+VERSION = '1.8.2'
 PACKAGE_NAME = 'pyDolarVenezuela' 
 AUTHOR = 'Francisco Griman'
 AUTHOR_EMAIL = 'grihardware@gmail.com'
