4d9246e747e2f9187ccadf9c4abee300b41f2a10
Francisco Griman <grihardware@gmail.com>
2024-08-02 16:50:52 -0400

    Bump version to 1.8.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '1.8.5'
+VERSION = '1.8.6'
 PACKAGE_NAME = 'pyDolarVenezuela' 
 AUTHOR = 'Francisco Griman'
 AUTHOR_EMAIL = 'grihardware@gmail.com'
