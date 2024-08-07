2056653a6950c241fdfdcb7a792156d6d72e1f80
Francisco Griman <grihardware@gmail.com>
2024-08-07 16:43:02 -0400

    Update version to 1.9.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '1.8.9'
+VERSION = '1.9.0'
 PACKAGE_NAME = 'pyDolarVenezuela' 
 AUTHOR = 'Francisco Griman'
 AUTHOR_EMAIL = 'grihardware@gmail.com'
