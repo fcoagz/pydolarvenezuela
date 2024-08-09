aa8918bf5ff61ba214abe0b0e157aa80c18eaa1c
Francisco Griman <grihardware@gmail.com>
2024-08-09 01:43:17 -0400

    Update conversion

--- pyDolarVenezuela/utils/calculator.py
+++ pyDolarVenezuela/utils/calculator.py
@@ -1,11 +1,11 @@
 from typing import Literal, Union
 from ..models import Monitor
 
-def currency_converter(type: Literal['VES', 'USD', 'EUR'], value, monitor: Monitor) -> Union[float, None]:
+def currency_converter(type: Literal['VES', 'USD', 'EUR'], value, monitor: Union[Monitor, dict]) -> Union[float, None]:
     """
     Convierte una cantidad de dinero de una moneda a otra utilizando los datos de un monitor específico.
     """
-    price_monitor = monitor.price
+    price_monitor = monitor.price if isinstance(monitor, Monitor) else monitor.get('price', None)
     if not price_monitor:
         raise KeyError('The monitor was not found')
 
