b3de50ab3ec6df71c88fc70ce23ec51e59f49c93
Francisco Griman <grihardware@gmail.com>
2024-10-15 01:25:23 -0400

    Bump version to 1.9.9

--- pyDolarVenezuela/version.py
+++ pyDolarVenezuela/version.py
@@ -2,7 +2,7 @@ import json
 from colorama import Fore
 from . import network
 
-__version__ = '1.9.8'
+__version__ = '1.9.9'
 """
 Versión actual de la biblioteca    
 """
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '1.9.8'
+VERSION = '1.9.9'
 PACKAGE_NAME = 'pyDolarVenezuela' 
 AUTHOR = 'Francisco Griman'
 AUTHOR_EMAIL = 'grihardware@gmail.com'
