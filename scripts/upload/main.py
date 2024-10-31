35b349f72e96643477d3c2f7b888f9d177324df7
Francisco Griman <grihardware@gmail.com>
2024-10-31 00:28:30 -0400

    Refactor image upload script to use a dictionary for existing images and improve data structure for JSON output

--- scripts/upload/main.py
+++ scripts/upload/main.py
@@ -2,7 +2,7 @@ import os
 import json
 import cloudinary
 from dotenv import load_dotenv
-from cloudinary.search import Search
+# from cloudinary.search import Search
 from cloudinary.uploader import upload
 from manifest import PAGES
 
@@ -23,21 +23,33 @@ def upload_image(file: bytes, public_id: str, folder: str) -> str:
     return upload_result["secure_url"]
 
 if __name__ == "__main__":
-    data = []
+    data = {}
     for page in PAGES:
         for file in page["files"]:
-            search = Search().expression(f"public_id:{os.path.basename(file['file']).split('.')[0]}").execute()
-            if search["total_count"] > 0:
-                url = search["resources"][0]["secure_url"]
-                print(f"Image {file['title']} already exists: {url}")
+            # search = Search().expression(f"public_id:{os.path.basename(file['file']).split('.')[0]}").execute()
+            # if search["total_count"] > 0:
+            #     url = search["resources"][0]["secure_url"]
+            #     print(f"Image {file['title']} already exists: {url}")
+            if data.get(file["title"]):
+                print(f"Image {file['title']} already exists: {data[file['title']]['image']}")
+                continue
             else:
                 with open(f"./icons/webp/{file['file']}", "rb") as image:
                     url = upload_image(image.read(), f'public_id:{os.path.basename(file['file']).split('.')[0]}', 'monitors')
                     print(f"Uploaded {file['title']}: {url}")
-            data.append({
+            data[file["title"]] = {
                 "provider": page["page"],
                 "title": file["title"],
                 "image": url
-            })
+            }
+    
+    data = [
+        {
+            "title": key,
+            "provider": value["provider"],
+            "image": value["image"]
+        }
+        for key, value in data.items()
+    ]
     with open("data.json", "w") as f:
         f.write(json.dumps(data, indent=4))
