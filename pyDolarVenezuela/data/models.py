3c4155c57b3037ecca66b99c2c48888a90438f41
Samuel Pavon <festovalros@gmail.com>
2025-01-16 22:35:03 -0400

    Update models.py according new guidelines of sqlalchemy to avoid warning

--- pyDolarVenezuela/data/models.py
+++ pyDolarVenezuela/data/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import String, Column, Integer, Float, DateTime, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
